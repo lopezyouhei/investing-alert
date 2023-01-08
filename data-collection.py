7a852942b5f03d6199da13a52fca075bfae313a8
Enrique <lopezyouhei@gmail.com>
2023-01-08 16:06:09 +0100

    update get_long function

--- data-collection.py
+++ data-collection.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
 import numpy as np
+import datetime
 
 # different price frequencies are required depending on the assessment
 # up to 200 day history with interval of 1-day is required for SMAs/RSI
@@ -13,4 +14,28 @@ class Assets:
         self.tickers = tickers
 
     def get_long(self):
-        pass
+        """Get 200 days worth of stock/crypto data for provided ticker
+        symbols, during initialization.
+
+        :param
+        None
+
+        :return
+        data: DataFrame table containing 200-days worth of price-close
+              data for each ticker.
+        """
+        price_data = {}
+        for ticker in self.tickers:
+            ticker_object = yf.Ticker(ticker)
+            ticker_close_price = ticker_object.history(start=datetime.date.today(),
+                                                       end=)
+            ticker_close_price = [["Date",
+                                   "Open",
+                                   "High",
+                                   "Low",
+                                   "Close"]].copy()
+
+
+ticker_list = ["aapl"]
+alert = Assets(ticker_list)
+print(alert)
